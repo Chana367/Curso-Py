b454f6828ee788cf77c31ac9f2b830e6240f717d
Francisco Paredes <paredesfrancisco031@gmail.com>
2023-10-16 17:47:35 -0300

    first commit

---
+++ tipos-de-datos/datosSimples.py
@@ -0,0 +1,20 @@
+# cadenas de texto
+"Hola mundo"
+'Hola'
+
+"""Hola mundo 
+          nombre: nombre
+          apellido:apellido"""
+
+'''Hola mundo 
+          nombre: nombre
+          apellido:apellido'''
+   
+#entero  
+40 
+# Float
+40.2
+
+# banderas
+True
+False
---
+++ variables/variables.py
@@ -0,0 +1,13 @@
+nombre = "Francisco"
+# concatenar con f-strings
+mensaje = f"hola {nombre} como estas?"
+
+#concatenar con + 
+
+mensaje = "hola " + nombre + " como estas?"
+
+
+print(mensaje)
+# operadores de pertenencia ( in / not in)
+print("Francisco" in mensaje)
+print("francisco" not in mensaje)
